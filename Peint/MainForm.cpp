d7531bb6d870de7c464a25a21c2ce898a5f4c68a
freezton <vlad.freezzy@gmail.com>
2022-10-24 01:48:41 +0300

    test

--- Peint/MainForm.cpp
+++ Peint/MainForm.cpp
@@ -1,11 +1,12 @@
 #include "MainForm.h"
 #include <Windows.h>
 #include <stdio.h>
+#include <time.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
-void main(array<String^>^ args)
+void main(array<String^>^ argv)
 {
 	Application::EnableVisualStyles;
 	Application::SetCompatibleTextRenderingDefault(false);
@@ -14,11 +15,40 @@ void main(array<String^>^ args)
 	Application::Run(% form);
 }
 
-System::Void Peint::MainForm::dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e)
+struct Point
 {
-	return System::Void();
+	int x, y;
+	Point(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
+class PointsArray
+{
+private:
+	int index = 0;
+
+};
+
+bool isMouseDown = false;
+
+System::Void Peint::MainForm::dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e) {}
+
+System::Void Peint::MainForm::domainUpDown1_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e) {}
+
+System::Void Peint::MainForm::pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
+{
+	isMouseDown = true;
 }
-System::Void Peint::MainForm::domainUpDown1_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e)
+
+System::Void Peint::MainForm::pictureBox1_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e)
 {
-	return System::Void();
+	isMouseDown = false;
+}
+
+System::Void Peint::MainForm::MainForm_Shown(System::Object^ sender, System::EventArgs^ e)
+{
+
 }
--- Peint/MainForm.h
+++ Peint/MainForm.h
@@ -18,9 +18,6 @@ namespace Peint {
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
 		}
 
 	protected:
@@ -35,12 +32,19 @@ namespace Peint {
 			}
 		}
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
 	protected:
 
-
-
 	protected:
-
 
 	private:
 		/// <summary>
@@ -56,7 +60,18 @@ namespace Peint {
 		void InitializeComponent(void)
 		{
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// pictureBox1
@@ -64,9 +79,119 @@ namespace Peint {
 			this->pictureBox1->Dock = System::Windows::Forms::DockStyle::Left;
 			this->pictureBox1->Location = System::Drawing::Point(0, 0);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(837, 561);
+			this->pictureBox1->Size = System::Drawing::Size(831, 511);
 			this->pictureBox1->TabIndex = 0;
 			this->pictureBox1->TabStop = false;
+			this->pictureBox1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::pictureBox1_MouseDown);
+			this->pictureBox1->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MainForm::pictureBox1_MouseUp);
+			// 
+			// panel1
+			// 
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel1->Controls->Add(this->button9);
+			this->panel1->Controls->Add(this->button8);
+			this->panel1->Controls->Add(this->button7);
+			this->panel1->Controls->Add(this->button6);
+			this->panel1->Controls->Add(this->button5);
+			this->panel1->Controls->Add(this->button4);
+			this->panel1->Controls->Add(this->button3);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(831, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(153, 111);
+			this->panel1->TabIndex = 1;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(7, 79);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(138, 23);
+			this->button9->TabIndex = 8;
+			this->button9->Text = L"Other colors";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->BackColor = System::Drawing::Color::Black;
+			this->button8->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button8->Location = System::Drawing::Point(115, 43);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(30, 30);
+			this->button8->TabIndex = 7;
+			this->button8->UseVisualStyleBackColor = false;
+			// 
+			// button7
+			// 
+			this->button7->BackColor = System::Drawing::Color::Fuchsia;
+			this->button7->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button7->Location = System::Drawing::Point(79, 43);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(30, 30);
+			this->button7->TabIndex = 6;
+			this->button7->UseVisualStyleBackColor = false;
+			// 
+			// button6
+			// 
+			this->button6->BackColor = System::Drawing::Color::Blue;
+			this->button6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button6->Location = System::Drawing::Point(43, 43);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(30, 30);
+			this->button6->TabIndex = 5;
+			this->button6->UseVisualStyleBackColor = false;
+			// 
+			// button5
+			// 
+			this->button5->BackColor = System::Drawing::Color::Lime;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Location = System::Drawing::Point(115, 7);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(30, 30);
+			this->button5->TabIndex = 4;
+			this->button5->UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::Cyan;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Location = System::Drawing::Point(7, 43);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(30, 30);
+			this->button4->TabIndex = 3;
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::Yellow;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Location = System::Drawing::Point(79, 7);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(30, 30);
+			this->button3->TabIndex = 2;
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Location = System::Drawing::Point(43, 7);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(30, 30);
+			this->button2->TabIndex = 1;
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Red;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point(7, 7);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(30, 30);
+			this->button1->TabIndex = 0;
+			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// MainForm
 			// 
@@ -74,7 +199,8 @@ namespace Peint {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
 			this->BackColor = System::Drawing::SystemColors::Control;
-			this->ClientSize = System::Drawing::Size(984, 561);
+			this->ClientSize = System::Drawing::Size(984, 511);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->pictureBox1);
 			this->ForeColor = System::Drawing::SystemColors::ControlText;
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
@@ -82,12 +208,17 @@ namespace Peint {
 			this->Name = L"MainForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"MainForm";
+			this->Shown += gcnew System::EventHandler(this, &MainForm::MainForm_Shown);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel1->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	private: System::Void dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void domainUpDown1_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e);
-	};
+		private: System::Void dateTimePicker1_ValueChanged(System::Object^ sender, System::EventArgs^ e);
+		private: System::Void domainUpDown1_SelectedItemChanged(System::Object^ sender, System::EventArgs^ e);
+		private: System::Void pictureBox1_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e);
+		private: System::Void pictureBox1_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e);
+		private: System::Void MainForm_Shown(System::Object^ sender, System::EventArgs^ e);
+};
 }
